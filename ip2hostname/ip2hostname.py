bb297b62f1eb2049f9a66aee00e2a2abf5853d7e
jeff <jeff@jmatt.io>
2019-09-30 12:09:59 -0400

    added ips and hostnames on one line

--- ip2hostname/ip2hostname.py
+++ ip2hostname/ip2hostname.py
@@ -3,4 +3,4 @@ import socket
 with open("ips.txt") as ips:
     for i in ips:
         i = i.replace("\n", "")
-        print (socket.gethostbyaddr(i)[0])
+        print (i + " , " +socket.gethostbyaddr(i)[0])
